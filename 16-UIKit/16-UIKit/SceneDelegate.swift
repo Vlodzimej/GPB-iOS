1cdb9fa092444baf774cbc21d8c99ad62ebd9f33
Амелькин Владимир <amelkin@vbankcenter.ru>
2022-11-02 23:03:54 +0300

    Fixed 16-UIKit

--- 16-UIKit/16-UIKit/SceneDelegate.swift
+++ 16-UIKit/16-UIKit/SceneDelegate.swift
@@ -10,12 +10,8 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-    var viewController: UIViewController?
+    weak var viewController: UIViewController?
     
-    private lazy var vc: ViewController? = {
-        viewController as? ViewController
-    }()
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
@@ -53,14 +49,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
-        vc?.createTimer()
+        guard let vc = viewController as? ViewController else { return }
+        vc.createTimer()
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
-        vc?.invalidateTimer()
+        guard let vc = viewController as? ViewController else { return }
+        vc.invalidateTimer()
     }
 
 
